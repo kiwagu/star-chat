e16415497dfbf05025cbc36afc3ea35405ce36d0
Kiwagu <kiwagu@gmail.com>
2020-10-29 11:53:21 +0100

    ADDED: using validation pipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,27 @@
 import 'dotenv/config';
 
+import { ValidationPipe } from '@nestjs/common';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /**
+   * Apply validation for all inputs globally
+   */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /**
+       * Strip away all none-object existing properties
+       */
+      whitelist: true,
+      /***
+       * Transform input objects to their corresponding DTO objects
+       */
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
