3dca709eadcf935eb9717bbc8b8b09190065a7f9
Kiwagu <kiwagu@gmail.com>
2020-10-29 16:30:52 +0100

    ADDED: react bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-import './App.css';
+import './App.scss';
 
 function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Register</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
