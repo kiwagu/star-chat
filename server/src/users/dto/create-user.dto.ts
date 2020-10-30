5c252fc5960ee04fba3701885434dd92eaf93e81
Kiwagu <kiwagu@gmail.com>
2020-10-30 13:45:58 +0100

    ADDED: custom match validator

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,4 +1,34 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+function Match(property: string, validationOptions?: ValidationOptions) {
+  return (object: any, propertyName: string) => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: MatchConstraint,
+    });
+  };
+}
+
+@ValidatorConstraint({ name: 'Match' })
+class MatchConstraint implements ValidatorConstraintInterface {
+  validate(value: any, args: ValidationArguments) {
+    const [relatedPropertyName] = args.constraints;
+    const relatedValue = (args.object as any)[relatedPropertyName];
+    return value === relatedValue;
+  }
+}
 
 /**
  * The CreateUserDto class is used to pass the information provided by the user upon registering a new account.
@@ -11,6 +41,11 @@ export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
   readonly password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Match('password', { message: 'passwords do not match' })
+  readonly confirmPassword: string;
 
   @IsNotEmpty()
   @IsEmail()
