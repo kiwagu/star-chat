6e20cde8b5930dfd001d3c7048ad067b37c816df
Kiwagu <kiwagu@gmail.com>
2020-10-30 15:26:58 +0100

    ADDED: init home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/* eslint-disable-next-line */
+export interface HomeProps {}
+
+export default function Home(props: HomeProps) {
+  return (
+    <div>
+      <h1>Welcome home!</h1>
+    </div>
+  );
+}
