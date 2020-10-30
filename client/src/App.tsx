db2b0675d5b902741484d3e1ae4cf3149559429d
Kiwagu <kiwagu@gmail.com>
2020-10-30 14:11:55 +0100

    ADDED: routing to react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 import './App.scss';
 import Register from './pages/Register';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Container className="pt-5">
-      <Register />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Register} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </Container>
   );
 }
