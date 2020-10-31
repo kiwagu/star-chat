f3e5366494449ddb0f6b38de98f12449bd09f19b
Kiwagu <kiwagu@gmail.com>
2020-10-31 19:17:01 +0100

    ADDED: navbar

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Button, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import { useAuthDispatch } from '../context/auth';
 
 /* eslint-disable-next-line */
 export interface HomeProps {}
 
 export default function Home(props: HomeProps) {
+  const authDispatch = useAuthDispatch();
+  const logout = () => {
+    authDispatch({ type: 'LOGOUT' });
+    window.location.href = '/login';
+  };
   return (
-    <div>
-      <h1>Welcome home!</h1>
-    </div>
+    <Row className="bg-white justify-content-around mb-1">
+      <Link to="/login">
+        <Button variant="link">Login</Button>
+      </Link>
+      <Link to="/register">
+        <Button variant="link">Register</Button>
+      </Link>
+      <Button variant="link" onClick={logout}>
+        Logout
+      </Button>
+    </Row>
   );
 }
