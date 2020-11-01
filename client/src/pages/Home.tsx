a895660d92c34e3727a9f49f9c574c1a1d6928be
Kiwagu <kiwagu@gmail.com>
2020-11-01 14:04:38 +0100

    ADDED: fetching users

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import { SERVER_URL } from '../consts';
 import { useAuthDispatch } from '../context/auth';
 
 /* eslint-disable-next-line */
@@ -13,6 +14,35 @@ export default function Home(props: HomeProps) {
     authDispatch({ type: 'LOGOUT' });
     window.location.href = '/login';
   };
+  const accessToken = localStorage.getItem('accessToken');
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${SERVER_URL}/users`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then(async (response) => {
+        const data = await response.json();
+
+        // check for error response
+        if (!response.ok) {
+          // get error message from body or default to response status
+          const error = (data && data.message) || response.status;
+          return Promise.reject(error);
+        }
+        setUsers(data);
+      })
+      .catch((errors) => {
+        console.error(errors);
+      });
+  }, [accessToken]);
+
+  console.log(users);
+  
+
   return (
     <Row className="bg-white justify-content-around mb-1">
       <Link to="/login">
